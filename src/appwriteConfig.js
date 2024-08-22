720a7fdbf7369b059b06557d01ac1dce98177e11
Devkarpenter <deveshkarpenter1998@gmail.com>
2024-08-22 12:35:09 +0530

    some changes

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,18 +1,14 @@
-import { Client, Databases,Account } from 'appwrite';
-
-
-export const PROJECT_ID = '66c06fe2002e7beb2af7'
-export const DATABASE_ID = '66c0715b00232b82fe5d'
-export const COLLECTION_ID_MESSAGES = '66c07178001e1f0a3d27'
-
+import { Client, Databases, Account } from 'appwrite';
 
 const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66c06fe2002e7beb2af7');
+    .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
-export const databases = new Databases(client);   
-export const account = new Account(client) 
+export const databases = new Databases(client);
+export const account = new Account(client);
 
-export default client;   
+export default client;
+
+ 
